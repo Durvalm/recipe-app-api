dfd303b9fa99c68c3bb1647f829510ae45d7f0ae
Durvz Maia <89949017+Durvalm@users.noreply.github.com>
2022-06-15 17:55:04 -0400

    configured database, tests for database setup, and CI/CD

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add X and Y and return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract X and Y and return result"""
+    return x - y
